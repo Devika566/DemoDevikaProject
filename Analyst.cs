581bde789f755e21583bff6e379e07cdff18f3ff
Devika566 <avulasettydevika2001@gmail.com>
2025-02-12 14:56:21 +0530

    Analyst Demo File Saved

---
+++ Analyst.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConApp2
+{
+    internal class Analyst:Employee
+    {
+        //For 'Analyst' class object is created in the program.cs file.
+        public double ClientAllowance = 10000;
+        public override double CalculateSalary()
+        {
+            double salaryPerMonth=ClientAllowance+base.CalculateSalary();
+            return salaryPerMonth;
+        }
+    }
+
+    /*
+     Base Keyword:Base keyword is used for access base class(Super class) method and base class constructor functionality from the derived class method and constructor.
+     
+     */
+}
